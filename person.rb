0b1fd4f2f7edc4afe4bd8cdfe87128d343e5cde5
kingqabil <kingqabil@live.com>
2022-03-16 18:56:52 +0300

    Files added and updated for associations

--- person.rb
+++ person.rb
@@ -1,5 +1,8 @@
+require_relative 'corrector'
+require_relative 'rental'
+
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals, :parent_permission
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -7,17 +10,16 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
+    @rentals = []
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   private
 
-  def of_age?
-    @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
+    @name = @corrector.correct_name(@name)
   end
 end
