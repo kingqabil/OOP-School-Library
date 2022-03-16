4a60bc5043ef469d46745e83c2e00412142eebc6
kingqabil <kingqabil@live.com>
2022-03-16 19:12:58 +0300

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
 require_relative 'corrector'
+require_relative 'rental'
 
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals, :parent_permission
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -10,26 +11,15 @@ class Person
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
+    @rentals = []
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   private
 
-  def of_age?
-    @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  def validate_name
     @name = @corrector.correct_name(@name)
   end
 end
-
-person = Person.new(35, 'SadiqHabil')
-person.validate_name
-
-puts person.name
